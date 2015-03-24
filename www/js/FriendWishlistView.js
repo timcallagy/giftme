98f1762879a79ae92b1b4206c421ce141ea8d72a
timcallagy <timcallagy@gmail.com>
2015-03-24 20:48:16 +0000

    Either Crowdfund or ChipIn button will be shown

--- www/js/FriendWishlistView.js
+++ www/js/FriendWishlistView.js
@@ -4,6 +4,14 @@ var FriendWishlistView = function (service, id) {
 
     this.initialize = function() {
         this.$el = $('<div/>');
+        Handlebars.registerHelper("check_if_zero", function(crowdfunded, options){
+            console.log(crowdfunded);
+            if (crowdfunded === 0) {
+                return options.fn(this);
+            } else {
+                return options.inverse(this);
+            }
+        });
     };
 
     this.render = function() {
