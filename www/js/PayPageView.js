566be256a4536c036703cf4db6705589be15fc1b
timcallagy <timcallagy@gmail.com>
2015-03-25 22:39:18 +0000

    Tidied up payment interface

--- www/js/PayPageView.js
+++ www/js/PayPageView.js
@@ -7,8 +7,20 @@ var PayPageView = function (service, id, pk) {
     };
 
     this.render = function() {
-        this.$el.html(this.template({id: id, pk: pk}));
-        return this;
+        url = "https://giftmeserver.herokuapp.com/get_gift/";
+        //url = "http://127.0.0.1:8000/get_gift/";
+        $.get(url + pk + "/", function( data ) {
+            data = JSON.parse(data);
+            friends = window.localStorage.getItem("friends");
+            friends = JSON.parse(friends);
+            for (var f in friends) {
+                if (friends[f].id == data[0].fields.owner_id){
+                    friend = friends[f];
+                }
+            }
+            self.$el.html(self.template({id: id, pk: pk, gift: data[0].fields, friend: friend}));
+            return this;
+        });
     };
     this.initialize();
 
