f9e0f238dcac1768cb152d0413d39a115b5292f7
timcallagy <timcallagy@gmail.com>
2015-03-27 20:04:43 +0000

    Wakeup message to server

--- www/js/LoginView.js
+++ www/js/LoginView.js
@@ -4,6 +4,19 @@ var LoginView = function (service) {
 
     this.initialize = function() {
         this.$el = $('<div/>');
+        // Send a GET request to heroku to wake up the dyno and avoid delays on the handset on other screens.
+        url = 'https://giftmeserver.herokuapp.com/wakeup/';
+        //url = 'http://127.0.0.1:8000/wakeup/';
+        $.ajax({
+            url: url,
+            type: 'get',
+            success: function(data) {
+                console.log('Success');
+            },
+            error: function() {
+                console.log('Error');
+            }
+        });
         openFB.init({appId: '1533444716908405'});
         openFB.api({
             path: '/me',
