39c23f54f58cd42a4762554360424e21f5d2c6bd
timcallagy <timcallagy@gmail.com>
2015-04-10 22:06:02 +0100

    Tidying up

--- www/js/HomeView.js
+++ www/js/HomeView.js
@@ -2,41 +2,11 @@ var HomeView = function (service) {
 
     self = this;
 
-    var getStatus = function () {
-        //        facebookConnectPlugin.getLoginStatus(
-        //              function (response) {
-        //            },
-        //          function (response) { 
-        //            console.log('error');
-        //          alert(response);
-        //        console.log(response);
-        //   });
-    }
-
     this.initialize = function() {
         this.$el = $('<div/>');
-        /*
-           var checkFB = function(){
-           if (typeof facebookConnectPlugin != 'undefined' && typeof FB != 'undefined'){
-           facebookConnectPlugin.login( ["email"],
-           function (response) { 
-           console.log('success!');
-        //window.location="#home/";
-        getStatus();
-        },
-        function (response) { 
-        window.location="#login/";
-        });
-        } else {
-        console.log('FB NOT READY');
-        setTimeout(checkFB, 500);
-        }
-        }
-        checkFB();
-        */
     };
+
     this.render = function() {
-        //        getStatus();
         if (typeof facebookConnectPlugin != 'undefined'){
             facebookConnectPlugin.api('/me', [],
                     function(response) {
@@ -52,43 +22,8 @@ var HomeView = function (service) {
                     );
         } else {
             console.log('facebookConnectPlugin not ready');
-            setTimeout(getStatus, 500);
+            setTimeout(self.render, 500);
         }
-
-
-
-        /*
-           openFB.api({
-           path: '/me',
-           success: function(data) {
-           userPic = 'http://graph.facebook.com/' + data.id + '/picture?type=small';
-           window.localStorage.setItem("id", data.id);
-           window.localStorage.setItem("my_name", data.first_name + " " + data.last_name);
-           self.$el.html(self.template(data));
-           return self;
-           },
-           error: function(data) {
-           openFB.login(
-           function(response) {
-           if(response.status === 'connected') {
-           window.localStorage.setItem("access_token", response.authResponse.token);
-           openFB.api({
-           path: '/me',
-           success: function(data) {
-           userPic = 'http://graph.facebook.com/' + data.id + '/picture?type=small';
-           window.localStorage.setItem("id", data.id);
-           window.localStorage.setItem("my_name", data.first_name + " " + data.last_name);
-           self.$el.html(self.template(data));
-           return self;
-           },
-           error: errorHandler});
-           } else {
-           alert('Facebook login failed: ' + response.error);
-           }
-           }, {scope: 'email,user_friends'});
-           }
-           });
-           */
     };
 
     this.initialize();
