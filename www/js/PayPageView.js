957cc2968468bfb360ddc911ac9a1bca38033115
timcallagy <timcallagy@gmail.com>
2015-04-16 23:01:55 +0100

    Look and feel of Pay Page

--- www/js/PayPageView.js
+++ www/js/PayPageView.js
@@ -10,14 +10,14 @@ var PayPageView = function (service, id, pk) {
     this.render = function() {
         $.get(backend_url + "get_gift/" + pk + "/", function( data ) {
             data = JSON.parse(data);
-            friends = window.localStorage.getItem("friends");
-            friends = JSON.parse(friends);
-            for (var f in friends) {
-                if (friends[f].id == data[0].fields.owner_id){
-                    friend = friends[f];
-                }
-            }
-            self.$el.html(self.template({friend_id: id, pk: pk, gift: data[0].fields, friend: friend}));
+            //friends = window.localStorage.getItem("friends");
+            //friends = JSON.parse(friends);
+            //for (var f in friends) {
+            //    if (friends[f].id == data[0].fields.owner_id){
+            //        friend = friends[f];
+            //    }
+            //}
+            self.$el.html(self.template({friend_id: id, pk: pk, gift: data[0].fields}));
             return this;
         });
     };
