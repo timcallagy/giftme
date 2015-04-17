fd744695b1e0e8a7ddd49fbc9b0117b5e4c97503
timcallagy <timcallagy@gmail.com>
2015-04-17 08:28:55 +0100

    Trying to fix Popover not closing

--- www/js/giftme.js
+++ www/js/giftme.js
@@ -247,10 +247,10 @@ $('body').on('click', function (e) {
         //the 'is' for buttons that trigger popups
         //the 'has' for icons within a button that triggers a popup
         if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
-            e.preventDefault();
-            alert('hiding');
             $(this).popover('hide');
         } else {
+            e.preventDefault();
+            alert('hiding');
             $(this).popover('hide');
         }
     });
