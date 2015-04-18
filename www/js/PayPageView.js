c8fc4057649f641f7a13d716e8e79e2d2cef4469
timcallagy <timcallagy@gmail.com>
2015-04-18 10:58:02 +0100

    Minor fixes and changes

--- www/js/PayPageView.js
+++ www/js/PayPageView.js
@@ -12,7 +12,9 @@ var PayPageView = function (service, id, pk) {
             data = JSON.parse(data);
             var amount = window.localStorage.getItem("amount");
             var message = window.localStorage.getItem("gift-message");
-            self.$el.html(self.template({friend_id: id, pk: pk, gift: data[0].fields, amount: amount, 'gift-message': message}));
+            var months = [{num: '1', name:'January'}, {num: '2', name:'February'}, {num: '3', name:'March'}, {num: '4', name:'April'}, {num: '5', name:'May'}, {num: '6', name:'June'}, {num: '7', name:'July'}, {num: '8', name:'August'}, {num: '9', name:'September'}, {num: '10', name:'October'}, {num: '11', name:'November'}, {num: '12', name:'December'}];
+            var years = [{num: '2015', name:'2015'}, {num: '2016', name:'2016'}, {num: '2017', name:'2017'}, {num: '2018', name:'2018'}, {num: '2019', name:'2019'}, {num: '2020', name:'2020'}, {num: '2021', name:'2021'}, {num: '2022', name:'2022'}, {num: '2023', name:'2023'}, {num: '2024', name:'2024'}]
+            self.$el.html(self.template({friend_id: id, pk: pk, gift: data[0].fields, amount: amount, 'gift-message': message, 'months': months, 'years': years}));
             return this;
         });
     };
