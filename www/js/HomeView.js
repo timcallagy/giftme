559f7c1a7ec7a72cade709244b51d96dff654f85
timcallagy <timcallagy@gmail.com>
2015-04-19 13:58:25 +0100

    Attempt to improve HomePage performance

--- www/js/HomeView.js
+++ www/js/HomeView.js
@@ -21,47 +21,70 @@ var HomeView = function (service) {
     this.render = function() {
         accessToken = window.localStorage.getItem("accessToken");
         userID = window.localStorage.getItem("id");
-        if (typeof facebookConnectPlugin != 'undefined'){
-            facebookConnectPlugin.api('/me', [],
-                    function(response) {
+        if (userID) {
+            console.log("USERID exists");
+            $.ajax({
+                url: backend_url + "get_notifications/" + userID + "/",
+                type: 'post',
+                data: {'accessToken': accessToken, 'userID': userID},
+                success: function(data) {
+                    data = JSON.parse(data);
+                    contributions_to = JSON.parse(data.contributions_to);
+                    contributions_from = JSON.parse(data.contributions_from);
+                    gifts = JSON.parse(data.gifts);
+                    birthdays = JSON.parse(data.birthdays);
+                    recent_friends = JSON.parse(data.recent_friends);
+                    var my_name = window.localStorage.getItem("my_name");
+                    self.$el.html(self.template({'my_name': my_name, 'contributions_to': contributions_to, 'contributions_from': contributions_from, 'recent_friends': recent_friends, 'gifts': gifts, 'birthdays': birthdays, 'my_id': userID}));
+                    return self;
+                },
+                error: function() {
+                    console.log('Error');
+                }
+            });
 
-                        $.ajax({
-                            url: backend_url + "get_notifications/" + userID + "/",
-                        type: 'post',
-                        data: {'accessToken': accessToken, 'userID': userID},
-                        success: function(data) {
-                            data = JSON.parse(data);
-                            contributions_to = JSON.parse(data.contributions_to);
-                            contributions_from = JSON.parse(data.contributions_from);
-                            gifts = JSON.parse(data.gifts);
-                            birthdays = JSON.parse(data.birthdays);
-                            recent_friends = JSON.parse(data.recent_friends);
+            } else {
+                console.log("USERID DOESN'T exist");
+                if (typeof facebookConnectPlugin != 'undefined'){
+                    facebookConnectPlugin.api('/me', [],
+                            function(response) {
 
-                            userPic = 'http://graph.facebook.com/' + response.id + '/picture?type=small';
-                            window.localStorage.setItem("id", response.id);
-                            window.localStorage.setItem("my_name", response.first_name + " " + response.last_name);
-                            self.$el.html(self.template({'profile': response, 'contributions_to': contributions_to, 'contributions_from': contributions_from, 'recent_friends': recent_friends, 'gifts': gifts, 'birthdays': birthdays}));
-                            return self;
-                        },
-                        error: function() {
-                            console.log('Error');
-                        }
-                        });
-                    },
-                        function(response) {
-                            console.log(response);
-                        }
-            );
-        } else {
-            console.log('facebookConnectPlugin not ready');
-            setTimeout(self.render, 500);
+                                $.ajax({
+                                    url: backend_url + "get_notifications/" + userID + "/",
+                                type: 'post',
+                                data: {'accessToken': accessToken, 'userID': userID},
+                                success: function(data) {
+                                    data = JSON.parse(data);
+                                    contributions_to = JSON.parse(data.contributions_to);
+                                    contributions_from = JSON.parse(data.contributions_from);
+                                    gifts = JSON.parse(data.gifts);
+                                    birthdays = JSON.parse(data.birthdays);
+                                    recent_friends = JSON.parse(data.recent_friends);
+                                    window.localStorage.setItem("id", response.id);
+                                    window.localStorage.setItem("my_name", response.first_name + " " + response.last_name);
+                                    self.$el.html(self.template({'profile': response, 'contributions_to': contributions_to, 'contributions_from': contributions_from, 'recent_friends': recent_friends, 'gifts': gifts, 'birthdays': birthdays}));
+                                    return self;
+                                },
+                                error: function() {
+                                    console.log('Error');
+                                }
+                                });
+                            },
+                                function(response) {
+                                    console.log(response);
+                                }
+                    );
+                } else {
+                    console.log('facebookConnectPlugin not ready');
+                    setTimeout(self.render, 500);
+                }
+            }
+        };
+
+        this.initialize();
+
+        function errorHandler(error) {
+            alert(error.message);
         }
-    };
 
-    this.initialize();
-
-    function errorHandler(error) {
-        alert(error.message);
     }
-
-}
